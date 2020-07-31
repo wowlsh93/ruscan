443e1b5df2a146892a4c3cfff216416e0ed1fcba
hama <wowlsh93@gmail.com>
2020-07-31 23:23:24 +0900

    [ADD] json rpc call

--- src/crawler/core.rs
+++ src/crawler/core.rs
@@ -7,6 +7,30 @@ use crossbeam::crossbeam_channel::select;
 use std::thread::{sleep, Thread};
 use std::sync::Arc;
 
+#[macro_use] extern crate hyper;
+extern crate rustc_serialize;
+extern crate url;
+
+use std::io::Read;
+use hyper::{Client};
+use hyper::header::{Headers, ContentType};
+use rustc_serialize::{Encodable, json};
+use rustc_serialize::json::Object;
+
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Param {
+    num: String,
+    detail: bool,
+}
+#[derive(RustcDecodable, RustcEncodable)]
+struct RpcRequest {
+    jsonrpc: String,
+    method: String,
+    params: params: Vec<Json>,
+    id: String,
+}
+
 pub struct Crawler {
 
     pub sender_ : Sender<String>,
@@ -36,7 +60,25 @@ impl Crawler {
         return c;
     }
 
+    fn post_json<T>(url: &str, payload: &T) -> hyper::Result<String>
+        where T: Encodable {
+        let client = Client::new();
+        let body = json::encode(payload).unwrap();
+        let mut response = try!(client.post(url).header(ContentType::json()).body(&body[..]).send());
+        let mut buf = String::new();
+        try!(response.read_to_string(&mut buf));
+        Ok(buf)
+    }
+
     fn getBlock(){
+
+        let request = RpcRequest {
+            jsonrpc: "2.0".to_owned(),
+            method: "eth_getBlockByNumber".to_owned(),
+            params: params: vec![Json::String("1".into()), Json::Boolean(true)],
+            id: "1".to_owned(),
+        };
+        println!("{}", post_json("https://mainnet.infura.io/v3/9366572d83da40e4b827a664e6194e06", &request).unwrap());
 
     }
 }
